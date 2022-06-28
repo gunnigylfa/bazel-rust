0b5551304ea333f9a82b2f5d7c28c6c9b8f2e6f4
Gunnar Gylfason <gunnargylfa@gmail.com>
2022-06-29 00:41:08 +0200

    Use cargo raze instead of crate universe (#3)

--- applications/helloworld/src/main.rs
+++ applications/helloworld/src/main.rs
@@ -1,8 +1,13 @@
 extern crate greeter_lib;
+extern crate log;
+extern crate rand;
 
 use greeter_lib::greeter;
+use rand::Rng;
 
 fn main() {
+    env_logger::init();
+    log::info!("About to say hello");
     let hello = greeter::Greeter::new("Hello");
     hello.greet("world");
 }
